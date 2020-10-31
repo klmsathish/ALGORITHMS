0ff9d0fe06adf27af30b81d797bf079036bc0d54
M Sathish kumar <klmsathish@gmail.com>
2020-10-31 16:40:15 +0530

    day - 9 commit

---
+++ CA/CA - 4/Mail-Merge-Go-main/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"io"
+	"os"
+	"text/template"
+)
+
+type TWrap struct{ Fields *[]string }
+
+func main() {
+	csvFile := flag.String("c", "list.csv", "The csv file to parse")                   //O(1)-T O(1)-S
+	templateName := flag.String("t", "report.tpl", "The template to use")              //O(1)-T O(1)-S
+	fileNameColumn := flag.Int("i", 0, "Column of csv to use as output file basename") //O(1)-T O(1)-S
+	fileExt := flag.String("s", "eml", "Output file suffix")                           //O(1)-T O(1)-S
+	flag.Parse()                                                                       //O(1)-T O(n)-S
+
+	template, err := template.ParseFiles(*templateName) //O(1)-T O(n)-S n - size of the template
+	if err != nil {
+		panic(err)
+	} //O(1)-T O(1)-S
+
+	file, err := os.Open(*csvFile) //O(1)-T O(n)-S
+	if err != nil {
+		panic(err)
+	} //O(1)-T O(1)-S
+	defer file.Close() //O(1)-T O(1)-S note space is released by the garbage collector
+	reader := csv.NewReader(file) //O(1)-T O(n)-S
+	reader.Comma = ','
+	for {
+		row, err := reader.Read() //O(1)-T O(n)-S number of column
+		if err == io.EOF {
+			break //O(1)-T O(1)-S
+		} else if err != nil {
+			panic(err)
+		} //O(1)-T O(1)-S
+		f, err := os.Create("./output/" + row[*fileNameColumn] + "." + *fileExt) //O(1)-T O(1)-S
+
+		defer f.Close()                          //O(1)-T O(1)-S note space is released by the garbage collector
+		template.Execute(f, TWrap{Fields: &row}) //O(1)-T O(m)-S m - number of template slots
+	} //O(n)-T O(n+m)-S
+	sendEmail()
+}
---
+++ CA/CA - 4/Mail-Merge-Go-main/report.tpl
@@ -0,0 +1,5 @@
+Course Report CSE -  270
+
+        Name:  {{index .Fields 2}} {{index .Fields 1}}
+        Unique ID:       {{index .Fields 4}}
+        Final Score: {{index .Fields 3}}/100%
